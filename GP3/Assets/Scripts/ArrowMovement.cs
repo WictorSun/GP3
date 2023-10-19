ecccd15af28287840542196a6ea51846bd07106b
Viktor Palmén <viktor.palmen23@gmail.com>
2023-10-19 14:38:11 +0200

    Updated Arrow Prefab

--- GP3/Assets/Scripts/ArrowMovement.cs
+++ GP3/Assets/Scripts/ArrowMovement.cs
@@ -6,25 +6,22 @@ using UnityEngine.UIElements;
 
 public class ArrowMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+    //public Rigidbody rb;
 
     PlayerInput playerInput;
 
     InputAction moveAction;
 
     [SerializeField] float horizontalSpeed = 10;
-
     [SerializeField] float tiltSpeed = 5;
     [SerializeField] float spinSpeed = 0;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
 
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
-
-
     }
 
     private void Update()
@@ -37,14 +34,21 @@ public class ArrowMovement : MonoBehaviour
         Vector2 direction = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, 0) * horizontalSpeed * Time.deltaTime;
 
+        // Log debug information when "a" or "d" is pressed.
+        if (direction.x < 0)
+        {
+            //Debug.Log("Pressed 'a' key. Moving left.");
+        }
+        else if (direction.x > 0)
+        {
+            //Debug.Log("Pressed 'd' key. Moving right.");
+        }
+
         float z = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Horizontal");
         Vector3 euler = transform.localEulerAngles;
         euler.z = Mathf.Lerp(euler.z, z, spinSpeed * Time.deltaTime);
         euler.y = Mathf.Lerp(euler.y, y, tiltSpeed);
         transform.localEulerAngles = euler;
-
-
-
     }
 }

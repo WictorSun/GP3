60676be8060d5d4489d3cd01477a4611dc526e34
Viktor Palmén <viktor.palmen23@gmail.com>
2023-10-26 11:13:14 +0200

    Updates Distance to decrease with speed

--- GP3/Assets/Scripts/Viktor test/UIController.cs
+++ GP3/Assets/Scripts/Viktor test/UIController.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private float timeMod = 4.0f; // Time modifier for distance
+    [SerializeField] private float timeMod = 2.0f; // Time modifier for distance
     [SerializeField] private TextMeshProUGUI distanceText = null; // Text for distance
     [SerializeField] private GameObject ReplayButton;
     [SerializeField] private GameObject winningScreen;
@@ -20,13 +20,15 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float modifiedTimeMod = timeMod + SpeedModifier.speed;  // Add the speed from SpeedModifier
+
         if (GameController.IncreaseDistance)
         {
-            GameController.Distance += Time.deltaTime * timeMod; // Increase distance by time passed
+            GameController.Distance += Time.deltaTime * modifiedTimeMod; // Increase distance by time passed
         }
         else
         {
-            GameController.Distance -= Time.deltaTime * timeMod; // Decrease distance by time passed
+            GameController.Distance -= Time.deltaTime * modifiedTimeMod; // Decrease distance by time passed
         }
 
         distanceText.text = String.Format("{0:0m}", GameController.Distance);
@@ -36,7 +38,6 @@ public class UIController : MonoBehaviour
             //GameController.EndGame();
             Time.timeScale = 0;
             ScoreCounter.Instance.WinningScoreCounter();
-            winningScreen.SetActive(true);
             winningScreen.SetActive(true);
         }
     }

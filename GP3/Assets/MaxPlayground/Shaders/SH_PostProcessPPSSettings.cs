d1e8f1ed991515b0359ea64e880e89946bbc9072
Nivavavava <max.niva@gmail.com>
2023-11-01 14:21:13 +0100

    Postprocess tweeks
    yes

--- GP3/Assets/MaxPlayground/Shaders/SH_PostProcessPPSSettings.cs
+++ GP3/Assets/MaxPlayground/Shaders/SH_PostProcessPPSSettings.cs
@@ -14,11 +14,11 @@ public sealed class SH_PostProcessPPSSettings : PostProcessEffectSettings
 	[Tooltip( "Texture Sample 1" )]
 	public TextureParameter _TextureSample2 = new TextureParameter {  };
 	[Tooltip( "Tile_U" )]
-	public FloatParameter _Tile_U2 = new FloatParameter { value = 8f };
+	public FloatParameter _Tile_U2 = new FloatParameter { value = 16f };
 	[Tooltip( "Tile_V" )]
-	public FloatParameter _Tile_V2 = new FloatParameter { value = 0.25f };
+	public FloatParameter _Tile_V2 = new FloatParameter { value = 0.1f };
 	[Tooltip( "Pan_V" )]
-	public FloatParameter _Pan_V1 = new FloatParameter { value = -0.5f };
+	public FloatParameter _Pan_V1 = new FloatParameter { value = -0.25f };
 	[Tooltip( "Pan_U" )]
 	public FloatParameter _Pan_U1 = new FloatParameter { value = 0.12f };
 	[Tooltip( "Fraction" )]
@@ -33,6 +33,14 @@ public sealed class SH_PostProcessPPSSettings : PostProcessEffectSettings
 	public FloatParameter _Switch = new FloatParameter { value = 1f };
 	[Tooltip( "PerspectiveCenter" )]
 	public Vector4Parameter _PerspectiveCenter = new Vector4Parameter { value = new Vector4(0.5f,0.77f,0f,0f) };
+	[Tooltip( "Desaturate_Edge" )]
+	public FloatParameter _Desaturate_Edge = new FloatParameter { value = 1f };
+	[Tooltip( "Brightness_Edge" )]
+	public FloatParameter _Brightness_Edge = new FloatParameter { value = 0.7f };
+	[Tooltip( "EdgeGradient_Edge" )]
+	public FloatParameter _EdgeGradient_Edge = new FloatParameter { value = 1f };
+	[Tooltip( "EdgeThreshold_Edge" )]
+	public FloatParameter _EdgeThreshold_Edge = new FloatParameter { value = 0.45f };
 }
 
 public sealed class SH_PostProcessPPSRenderer : PostProcessEffectRenderer<SH_PostProcessPPSSettings>
@@ -52,6 +60,10 @@ public sealed class SH_PostProcessPPSRenderer : PostProcessEffectRenderer<SH_Pos
 		sheet.properties.SetFloat( "_Contrast", settings._Contrast );
 		sheet.properties.SetFloat( "_Switch", settings._Switch );
 		sheet.properties.SetVector( "_PerspectiveCenter", settings._PerspectiveCenter );
+		sheet.properties.SetFloat( "_Desaturate_Edge", settings._Desaturate_Edge );
+		sheet.properties.SetFloat( "_Brightness_Edge", settings._Brightness_Edge );
+		sheet.properties.SetFloat( "_EdgeGradient_Edge", settings._EdgeGradient_Edge );
+		sheet.properties.SetFloat( "_EdgeThreshold_Edge", settings._EdgeThreshold_Edge );
 		context.command.BlitFullscreenTriangle( context.source, context.destination, sheet, 0 );
 	}
 }

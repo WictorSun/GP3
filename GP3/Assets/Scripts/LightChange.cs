afb6664527230d99f27477fbbb2f3fe60d1fb57c
Hugo Axelsson <72792915+Baxaren@users.noreply.github.com>
2023-11-13 15:08:15 +0100

    Trees out of view don't cast shadows, prefab changed

---
+++ GP3/Assets/Scripts/LightChange.cs
@@ -0,0 +1,29 @@
+using Autodesk.Fbx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChange : MonoBehaviour
+{
+    Light lt;
+    Color color0 = Color.blue;
+    Color color1 = Color.red;
+
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    public void ChangeToBlue()
+    {
+        lt.color = color0;
+    }
+
+    public void ChangeToRed()
+    {
+        lt.color = color1;
+    }
+
+
+}
